06239e8a60f07700b08b0bf61bbc96de2553682b
katarina161 <katarinanovakovich666@gmail.com>
2020-03-02 10:42:58 +0100

    Dodata klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica2020/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
