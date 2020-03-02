2f386ba9b093ac1d19b03702b353621e14cd948a
katarina161 <katarinanovakovich666@gmail.com>
2020-03-02 11:07:05 +0100

    Implementirana metoda dodajKurs

--- Menjacnica2020/src/menjacnica/Menjacnica.java
+++ Menjacnica2020/src/menjacnica/Menjacnica.java
@@ -1,25 +1,35 @@
 package menjacnica;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private List<Valuta> valute = new ArrayList<Valuta>();
 
 	@Override
-	public void dodajKurs(Kurs k) {
+	public void dodajKurs(String valuta, Kurs k) {
+		for(int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNazivValute().equals(valuta)) {
+				for(int j = 0; j < valute.get(i).getKursevi().size(); j++) {
+					if(valute.get(i).getKursevi().get(j).equals(k))
+						valute.get(i).getKursevi().remove(j);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void obrisiKurs(String valuta, Kurs k) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void obrisiKurs(Kurs k) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Kurs pronadjiKurs(GregorianCalendar datum) {
+	public Kurs pronadjiKurs(String valuta, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
 		return null;
 	}
